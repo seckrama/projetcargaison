5468e40f3f5e205582c998fae42b3ac5b6879859
Ramaseck1 <seckrama022@gmail.com>
2024-05-24 12:07:41 +0000

    first commit

---
+++ dist/app-bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+var text = 'Hello TypeScript';
+console.log(text);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLWJ1bmRsZS5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLElBQU0sSUFBSSxHQUFXLGtCQUFrQixDQUFDO0FBQ3hDLE9BQU8sQ0FBQyxHQUFHLENBQUMsSUFBSSxDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wcm9qZXR0c3BocC8uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCB0ZXh0OiBzdHJpbmcgPSAnSGVsbG8gVHlwZVNjcmlwdCc7XG5jb25zb2xlLmxvZyh0ZXh0KTtcblxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
